34ce1530e3df4251db2318bde0ac8fde2d7434c8
SergioFiloth <58532720+SergioFiloth@users.noreply.github.com>
2020-06-09 08:33:07 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
 (function(){
-    // Galeria con Swiper
-    let swiper = new Swiper(".swiper-container", {
-        slidesPerView: "auto",
-        loop: true,
-        spaceBetween: 10,
-        // disableOnInteraction: false,
-        navigation: {
-            prevEl: '.swiper-button-prev',
-            nextEl: '.swiper-button-next',
-        },
-        autoplay: {
-            delay: 1500,
-        },
-    })
     // Todas las funciones referentes al menu
     function menuToggle (){
         // Activacion y desactivacion de menu
@@ -32,4 +18,9 @@
         menu.addEventListener("click", dM)
     }
     menuToggle()
+    // const urlW = window.location.hash
+
+    // console.log(urlW)
+
+    // Funcion para cambiar la clase
 }())
